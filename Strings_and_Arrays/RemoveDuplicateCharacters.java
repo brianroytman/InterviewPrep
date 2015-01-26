da44815d8a44df07542df91da4675f1ccadb40a3
Brian Roytman <brian.roytman@gmail.com>
2015-01-26 12:33:47 -0500

    Removes all instances of a character if a duplicate is detected.

---
+++ Strings_and_Arrays/RemoveDuplicateCharacters.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class RemoveDuplicateCharacters {
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter a word to test for uniqueness: ");
+		String testString = input.nextLine();
+		String printRes = removeDuplicates(testString);
+		System.out.println(printRes);
+	}
+	
+	
+	public static String removeDuplicates(String str)
+	{
+		//char holder;
+		String adjustedStr1 = str.toLowerCase();
+		String adjustedStr2 = str.toLowerCase();
+		String temp = "";
+		int count = 0;
+		char currentChar;
+		char nextChar;
+		for (int i = 0; i < str.length(); i++)
+		{
+			currentChar = adjustedStr1.charAt(i);
+			for (int j = 0; j < adjustedStr2.length(); j++)
+			{
+				nextChar =  adjustedStr2.charAt(j);
+				if (currentChar == nextChar)
+				{
+					count++;
+				} 	 
+			}		
+			if (count <= 1)
+			{
+				temp = temp + currentChar;
+			}
+			count = 0;
+		}
+		
+		return temp;
+	}
+
+
+}
